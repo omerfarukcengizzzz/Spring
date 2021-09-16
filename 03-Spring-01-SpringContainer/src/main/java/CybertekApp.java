ac98e3c8af1a7d5e3b08cac6bbd575bf410c1060
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2021-09-17 00:21:20 +0100

    Spring Container

--- 03-Spring-01-SpringContainer/src/main/java/CybertekApp.java
+++ 03-Spring-01-SpringContainer/src/main/java/CybertekApp.java
@@ -1,7 +1,17 @@
+import interfaces.Mentor;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class CybertekApp {
     public static void main(String[] args) {
 
+//        BeanFactory container = new ClassPathXmlApplicationContext("config.xml");
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
 
+        Mentor mentor = (Mentor) container.getBean("partTimeMentor");
+
+        mentor.createAccount();
 
     }
 }
