a94ccc7180417b7c3b21ab1ef6c512f0b7e6200b
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2021-09-16 04:11:40 +0100

    Loosely Coupled

--- 02-Spring-00-LooselyCoupled/src/CybertekApp.java
+++ 02-Spring-00-LooselyCoupled/src/CybertekApp.java
@@ -1,13 +1,14 @@
-import implementation.Mentor;
 import service.FullTimeMentor;
 import service.MentorAccount;
+import service.PartTimeMentor;
 
 public class CybertekApp {
     public static void main(String[] args) {
 
-        FullTimeMentor fullTimeMentor = new FullTimeMentor();
+//        FullTimeMentor fullTimeMentor = new FullTimeMentor();
+        PartTimeMentor partTimeMentor = new PartTimeMentor();
 
-        MentorAccount mentorAccount = new MentorAccount(fullTimeMentor);
+        MentorAccount mentorAccount = new MentorAccount(partTimeMentor);
 
         mentorAccount.manageAccount();
 
