533bb742f571f40cd4e833a48b02ba7e7564285c
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2021-09-17 02:27:37 +0100

    DI - Constructor Injection

--- 04-Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/CybertekApp.java
+++ 04-Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/CybertekApp.java
@@ -1,13 +1,13 @@
 package com.cybertek;
 
 import com.cybertek.interfaces.Course;
-import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class CybertekApp {
     public static void main(String[] args) {
 
-        BeanFactory container = new ClassPathXmlApplicationContext("config.xml");
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
 
         Course course = container.getBean("java", Course.class);
 
