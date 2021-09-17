26da3ccb800749cabbe3f86df3e6012edcaec232
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2021-09-17 03:03:19 +0100

    DI - Setter Injection

---
+++ 05-Spring-02-DI-SetterInjection/src/main/java/com/cybertek/services/Java.java
@@ -0,0 +1,19 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
+
+public class Java implements Course {
+
+//    OfficeHours officeHours;
+    ExtraSessions extraSessions;
+
+    public Java(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
+
+    @Override
+    public void getTeachingHours() {
+        System.out.println("Weekly teaching hours = " + (20 + extraSessions.getHours()));
+    }
+}
