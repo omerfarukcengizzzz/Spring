d2f1707d76ba6823f0d460104237ba9312005632
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2021-09-21 07:17:01 +0100

    Java Code - Configuration, ComponentScan, Bean

--- 12-Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/configs/CybertekAppConfig.java
+++ 12-Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/configs/CybertekAppConfig.java
@@ -1,6 +1,8 @@
 package com.cybertek.configs;
 
+import com.cybertek.interfaces.ExtraSessions;
 import com.cybertek.services.Java;
+import com.cybertek.services.OfficeHours;
 import com.cybertek.services.Selenium;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,14 +15,27 @@ public class CybertekAppConfig {
     // The reason why we are creating beans in this class is, in some cases we might need to use
     // third party classes and in order to create beans from those classes, we will have to
     // create beans on method level in the Config class
+
     @Bean
     public Java java() {
-        return new Java();
+        return new Java(extraSessions());
     }
 
     @Bean
     public Selenium selenium() {
         return new Selenium();
+    }
+
+    /*
+        @Bean
+        public OfficeHours officeHours() {
+            return new OfficeHours();
+        }
+    */
+
+    @Bean
+    public ExtraSessions extraSessions() {  // ExtraSessions ex = new OfficeHours();
+        return new OfficeHours();
     }
 
 }
--- 12-Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Java.java
+++ 12-Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Java.java
@@ -1,10 +1,18 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
 
 public class Java implements Course {
+
+    private ExtraSessions extraSessions;
+
+    public Java(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
+
     @Override
     public void getTeachingHours() {
-        System.out.println("Weekly teaching hours = 30");
+        System.out.println("Weekly teaching hours = " + (30 + extraSessions.getHours()));
     }
 }
