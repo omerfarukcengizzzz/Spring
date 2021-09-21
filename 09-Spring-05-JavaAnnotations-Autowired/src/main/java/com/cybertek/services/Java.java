e885f409a5a3c5fb51c76444e1eed5b83d7afd40
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2021-09-21 04:27:43 +0100

    Java Annotations - Autowired

--- 09-Spring-05-JavaAnnotations-Autowired/src/main/java/com/cybertek/services/Java.java
+++ 09-Spring-05-JavaAnnotations-Autowired/src/main/java/com/cybertek/services/Java.java
@@ -8,8 +8,29 @@ import org.springframework.stereotype.Component;
 @Component
 public class Java implements Course {
 
+    // Field Injection
     @Autowired
     private ExtraSessions extraSessions;
+
+    /*
+        // Constructor Injection
+        // we can also do constructor injection with the help of lombok annotations(@AllArgsConstructor)
+        @Autowired  // we don't need to put @Autowired annotation if there is only one constructor
+        public Java(ExtraSessions extraSessions) {
+            this.extraSessions = extraSessions;
+        }
+    */
+
+    /*
+        // Setter Injection
+        // we can also do setter injection with the help of lombok annotations(@Setter)
+        @Autowired
+        public void setExtraSessions(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+        }
+    */
+
+
 
     @Override
     public void getTeachingHours() {
