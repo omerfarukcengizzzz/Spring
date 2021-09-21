0d79fb99294b45544794adc477f634d0293cb238
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2021-09-21 07:03:13 +0100

    Java Code - Configuration, ComponentScan, Bean

--- 12-Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Selenium.java
+++ 12-Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Selenium.java
@@ -1,9 +1,7 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
-import org.springframework.stereotype.Component;
 
-@Component
 public class Selenium implements Course {
     @Override
     public void getTeachingHours() {
