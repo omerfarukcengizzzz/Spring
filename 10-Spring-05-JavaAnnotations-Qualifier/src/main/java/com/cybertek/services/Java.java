f1b9fff73d2660771d79c4c19ce433dc4db5b6b8
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2021-09-21 04:49:37 +0100

    Java Annotations - Qualifier

--- 10-Spring-05-JavaAnnotations-Qualifier/src/main/java/com/cybertek/services/Java.java
+++ 10-Spring-05-JavaAnnotations-Qualifier/src/main/java/com/cybertek/services/Java.java
@@ -3,34 +3,20 @@ package com.cybertek.services;
 import com.cybertek.interfaces.Course;
 import com.cybertek.interfaces.ExtraSessions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Java implements Course {
-
-    // Field Injection
+    /*
     @Autowired
+    @Qualifier("officeHours")
+    */
     private ExtraSessions extraSessions;
 
-    /*
-        // Constructor Injection
-        // we can also do constructor injection with the help of lombok annotations(@AllArgsConstructor)
-        @Autowired  // we don't need to put @Autowired annotation if there is only one constructor
-        public Java(ExtraSessions extraSessions) {
-            this.extraSessions = extraSessions;
-        }
-    */
-
-    /*
-        // Setter Injection
-        // we can also do setter injection with the help of lombok annotations(@Setter)
-        @Autowired
-        public void setExtraSessions(ExtraSessions extraSessions) {
+    public Java(@Qualifier("officeHours") ExtraSessions extraSessions) {
         this.extraSessions = extraSessions;
-        }
-    */
-
-
+    }
 
     @Override
     public void getTeachingHours() {
--- 10-Spring-05-JavaAnnotations-Qualifier/src/main/java/com/cybertek/services/Selenium.java
+++ 10-Spring-05-JavaAnnotations-Qualifier/src/main/java/com/cybertek/services/Selenium.java
@@ -3,12 +3,14 @@ package com.cybertek.services;
 import com.cybertek.interfaces.Course;
 import com.cybertek.interfaces.ExtraSessions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Selenium implements Course {
 
     @Autowired
+    @Qualifier("mockInterviewHours")
     private ExtraSessions extraSessions;
 
     @Override
