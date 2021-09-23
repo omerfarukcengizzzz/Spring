43b45046376050c7f249ace9f81857bdadad3056
Omer Faruk <omerfarukcengizzzz@gmail.com>
2021-09-23 12:59:37 +0100

    Spring Boot - Demo

--- 14-Spring-07-SpringBoot-Demo/src/main/java/com/cybertek/Application.java
+++ 14-Spring-07-SpringBoot-Demo/src/main/java/com/cybertek/Application.java
@@ -3,7 +3,8 @@ package com.cybertek;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// if you add the scanBasePackages parameter, it will override the default scan package
+@SpringBootApplication(scanBasePackages = "com.cybertek")
 public class Application {
 
     public static void main(String[] args) {
