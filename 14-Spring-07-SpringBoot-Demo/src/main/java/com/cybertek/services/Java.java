48e279d18fabea15e201ddfa365091e48ba15ca6
Omer Faruk <omerfarukcengizzzz@gmail.com>
2021-09-23 15:04:16 +0100

    Spring Boot - Demo

--- 14-Spring-07-SpringBoot-Demo/src/main/java/com/cybertek/services/Java.java
+++ 14-Spring-07-SpringBoot-Demo/src/main/java/com/cybertek/services/Java.java
@@ -3,10 +3,14 @@ package com.cybertek.services;
 import com.cybertek.interfaces.Course;
 import com.cybertek.interfaces.ExtraSessions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Java implements Course {
+
+    @Value("${instructor}")
+    private String instructor;
 
     @Autowired
     private ExtraSessions extraSessions;
@@ -14,5 +18,6 @@ public class Java implements Course {
     @Override
     public void getTeachingHours() {
         System.out.println("Weekly Teaching Hours = " + (30 + extraSessions.getHours()));
+        System.out.println("instructor = " + instructor);
     }
 }
