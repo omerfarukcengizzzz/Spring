8949a98897f349a471275d8a5896907cfc915d4f
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2021-09-26 16:58:44 +0100

    Spring Boot Lab - Carpet Calculator

--- 15-Spring-07-SpringBoot-IoC-DI-Lab/src/main/java/com/cybertek/services/carpet/CarpetTX.java
+++ 15-Spring-07-SpringBoot-IoC-DI-Lab/src/main/java/com/cybertek/services/carpet/CarpetTX.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Component
 public class CarpetTX implements Carpet {
@@ -14,14 +15,19 @@ public class CarpetTX implements Carpet {
     public static final Map<City, BigDecimal> prices = new HashMap<>();
 
     static {
-        prices.put(City.SAN_ANTONIO, BigDecimal.valueOf(4.13));
-        prices.put(City.AUSTIN, BigDecimal.valueOf(3.22));
-        prices.put(City.DALLAS, BigDecimal.valueOf(5.99));
+        prices.put(City.SAN_ANTONIO, BigDecimal.valueOf(2.13));
+        prices.put(City.AUSTIN, BigDecimal.valueOf(1.22));
+        prices.put(City.DALLAS, BigDecimal.valueOf(1.99));
     }
 
     @Override
     public BigDecimal getSqFtPrice(City city) {
-        return prices.entrySet().stream()
-                .filter(x -> x.getKey() == city).findFirst().get().getValue();
+
+        BigDecimal defaultValue = BigDecimal.ZERO;
+
+        Optional<Map.Entry<City, BigDecimal>> collect = prices.entrySet().stream()
+                .filter(x -> x.getKey() == city).findFirst();
+
+        return collect.isPresent() ? collect.get().getValue() : defaultValue;
     }
 }
