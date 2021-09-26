6f1a93c6d1a629015a2fad8ecd710789adbf8915
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2021-09-26 16:55:12 +0100

    Spring Boot Lab - Carpet Calculator

--- 15-Spring-07-SpringBoot-IoC-DI-Lab/src/main/java/com/cybertek/services/carpet/CarpetTX.java
+++ 15-Spring-07-SpringBoot-IoC-DI-Lab/src/main/java/com/cybertek/services/carpet/CarpetTX.java
@@ -5,13 +5,23 @@ import com.cybertek.interfaces.carpetPrices.Carpet;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class CarpetTX implements Carpet {
 
+    public static final Map<City, BigDecimal> prices = new HashMap<>();
+
+    static {
+        prices.put(City.SAN_ANTONIO, BigDecimal.valueOf(4.13));
+        prices.put(City.AUSTIN, BigDecimal.valueOf(3.22));
+        prices.put(City.DALLAS, BigDecimal.valueOf(5.99));
+    }
 
     @Override
     public BigDecimal getSqFtPrice(City city) {
-        return null;
+        return prices.entrySet().stream()
+                .filter(x -> x.getKey() == city).findFirst().get().getValue();
     }
 }
