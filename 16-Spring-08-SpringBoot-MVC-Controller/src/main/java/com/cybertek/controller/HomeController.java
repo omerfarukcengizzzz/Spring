0d4825c091574c2920738b07570694026df49795
Omer Faruk <omerfarukcengizzzz@gmail.com>
2021-10-01 17:02:12 +0100

    Spring Boot - MVC - Controller

--- 16-Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
+++ 16-Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
@@ -1,10 +1,7 @@
 package com.cybertek.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
@@ -35,5 +32,12 @@ public class HomeController {
     public String postMapping() {
         return "home";
     }
+
+    @GetMapping("/home/{name}")
+    public String pathVariable(@PathVariable("name") String name) {
+        System.out.println("name = " + name);
+        return "home";
+    }
+
 
 }
