2b5fe4ee2c6c2b24920fdb556c74c64712ece59a
Omer Faruk <omerfarukcengizzzz@gmail.com>
2021-10-02 12:30:48 +0100

    Spring Boot - MVC - Controller

---
+++ 16-Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/MentorController.java
@@ -0,0 +1,14 @@
+package com.cybertek.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MentorController {
+
+    @GetMapping("/show")
+    public String showForm() {
+        return "home";
+    }
+
+}
