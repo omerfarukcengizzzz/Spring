225b1f92b069cec239e6fd4089ecfaee036f14e8
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2021-10-09 16:56:41 +0100

    Spring Boot - MVC - Model - View(Thymeleaf)

--- 17-Spring-08-SpringBoot-MVC-Model-View_Thymeleaf/src/main/java/com/cybertek/controller/StudentController.java
+++ 17-Spring-08-SpringBoot-MVC-Model-View_Thymeleaf/src/main/java/com/cybertek/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.cybertek.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,14 +10,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/student")
 public class StudentController {
 
+    @Value("${name}")
+    String name;
+
+    @Value("${year}")
+    Integer year;
+
     @GetMapping("/welcome")
     public String homePage(Model model) {
 
-        model.addAttribute("name", "Ozzy");
-        /*
-            Integer num = 1;
-            model.addAttribute(num);
-        */
+        model.addAttribute("name", name);
+        model.addAttribute("year", year);
+
         return "student/welcome";
     }
 
