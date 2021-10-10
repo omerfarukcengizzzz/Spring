e2cc3c6f748e5c22b669d878a14336c51d919d56
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2021-10-10 19:41:10 +0100

    Spring Boot - MVC - Model - View(Thymeleaf)

--- 17-Spring-08-SpringBoot-MVC-Model-View_Thymeleaf/src/main/java/com/cybertek/controller/MentorController.java
+++ 17-Spring-08-SpringBoot-MVC-Model-View_Thymeleaf/src/main/java/com/cybertek/controller/MentorController.java
@@ -1,8 +1,14 @@
 package com.cybertek.controller;
 
+import com.cybertek.enums.Gender;
+import com.cybertek.model.Mentor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/mentor")
@@ -13,4 +19,17 @@ public class MentorController {
         return "/mentor/welcome";
     }
 
+    @GetMapping("/list")
+    public String showTable(Model model) {
+
+        List<Mentor> mentorList = new ArrayList<>();
+        mentorList.add(new Mentor("Mike", "Smith", 45, Gender.MALE));
+        mentorList.add(new Mentor("Tom", "Hanks", 65, Gender.MALE));
+        mentorList.add(new Mentor("Amy", "Fowler", 33, Gender.FEMALE));
+        mentorList.add(new Mentor("Sheldon", "Cooper", 36, Gender.MALE));
+
+        model.addAttribute("mentors", mentorList);
+
+        return "/mentor/list";
+    }
 }
