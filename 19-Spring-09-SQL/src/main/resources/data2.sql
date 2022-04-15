a5bef0e0d0ed27138df09d028110c8c116f079e3
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-04-15 02:30:47 +0100

    SQL - Review

---
+++ 19-Spring-09-SQL/src/main/resources/data2.sql
@@ -0,0 +1,30 @@
+CREATE TABLE Towns
+(
+    id      SERIAL UNIQUE NOT NULL,
+    code    VARCHAR(10)   NOT NULL, -- not unique
+    article TEXT,
+    name    TEXT          NOT NULL  -- not unique
+);
+
+create index idx_towns_name on towns (name);
+drop index if exists idx_towns_name;
+
+insert into towns (code, article, name)
+select left(md5(i::text), 10),
+    md5(random()::text),
+    md5(random()::text)
+from generate_series(1, 1000000) s(i);
+
+explain analyze
+select *
+from towns
+where name = '4de3193755c5b5453eaf7080c9536663';
+
+select tablename,
+       indexname,
+       indexdef
+FROM pg_indexes
+WHERE schemaname = 'public'
+ORDER BY tablename,
+         indexname;
+
