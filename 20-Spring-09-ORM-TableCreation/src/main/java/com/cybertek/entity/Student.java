0cbcf83bb3176047d07f3b7fc74f1b926a3c16a1
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-04-17 21:59:21 +0100

    ORM - Table Creation

--- 20-Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
+++ 20-Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
@@ -1,14 +1,13 @@
 package com.cybertek.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "students")
 public class Student {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String firstName;
--- 20-Spring-09-ORM-TableCreation/src/main/resources/data.sql
+++ 20-Spring-09-ORM-TableCreation/src/main/resources/data.sql
@@ -1,11 +1,4 @@
-insert into students(id, first_name, last_name, email)
-values (1, 'Mike', 'Smith', 'mike@cybertek.com');
-
-insert into students(id, first_name, last_name, email)
-values (2, 'Adam', 'Smith', 'adam@cybertek.com');
-
-insert into students(id, first_name, last_name, email)
-values (3, 'Tom', 'Hanks', 'tomhanks@cybertek.com');
-
-insert into students(id, first_name, last_name, email)
-values (4, 'Victor', 'Navorsky', 'victor@cybertek.com');
+insert into students(first_name, last_name, email) values ('Mike', 'Smith', 'mike@cybertek.com');
+insert into students(first_name, last_name, email) values ('Adam', 'Smith', 'adam@cybertek.com');
+insert into students(first_name, last_name, email) values ('Tom', 'Hanks', 'tomhanks@cybertek.com');
+insert into students(first_name, last_name, email) values ('Victor', 'Navorsky', 'victor@cybertek.com');
