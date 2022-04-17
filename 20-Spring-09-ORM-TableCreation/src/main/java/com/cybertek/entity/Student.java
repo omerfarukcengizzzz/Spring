d954091f4d3d041a5d117437382d9327897ed8ad
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-04-17 22:03:04 +0100

    ORM - Table Creation

--- 20-Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
+++ 20-Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
@@ -10,6 +10,7 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "studentFirstName", length = 50, nullable = false)
     private String firstName;
     private String lastName;
     private String email;
--- 20-Spring-09-ORM-TableCreation/src/main/resources/data.sql
+++ 20-Spring-09-ORM-TableCreation/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into students(first_name, last_name, email) values ('Mike', 'Smith', 'mike@cybertek.com');
-insert into students(first_name, last_name, email) values ('Adam', 'Smith', 'adam@cybertek.com');
-insert into students(first_name, last_name, email) values ('Tom', 'Hanks', 'tomhanks@cybertek.com');
-insert into students(first_name, last_name, email) values ('Victor', 'Navorsky', 'victor@cybertek.com');
+insert into students(student_first_name, last_name, email) values ('Mike', 'Smith', 'mike@cybertek.com');
+insert into students(student_first_name, last_name, email) values ('Adam', 'Smith', 'adam@cybertek.com');
+insert into students(student_first_name, last_name, email) values ('Tom', 'Hanks', 'tomhanks@cybertek.com');
+insert into students(student_first_name, last_name, email) values ('Victor', 'Navorsky', 'victor@cybertek.com');
