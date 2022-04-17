eab757f4ef4dfba75d8690498d8114fd34dc5edc
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-04-17 21:13:06 +0100

    ORM - Table Creation

--- 20-Spring-09-ORM-TableCreation/src/main/resources/data.sql
+++ 20-Spring-09-ORM-TableCreation/src/main/resources/data.sql
@@ -1,2 +1,11 @@
 insert into student(id, first_name, last_name, email)
 values (1, 'Mike', 'Smith', 'mike@cybertek.com');
+
+insert into student(id, first_name, last_name, email)
+values (2, 'Adam', 'Smith', 'adam@cybertek.com');
+
+insert into student(id, first_name, last_name, email)
+values (3, 'Tom', 'Hanks', 'tomhanks@cybertek.com');
+
+insert into student(id, first_name, last_name, email)
+values (4, 'Victor', 'Navorsky', 'victor@cybertek.com');
