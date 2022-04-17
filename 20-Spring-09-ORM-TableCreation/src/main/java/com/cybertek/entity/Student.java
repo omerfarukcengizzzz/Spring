06c345e42d69847eab7037c1aab305a96cc8fb60
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-04-17 22:15:27 +0100

    ORM - Table Creation

--- 20-Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
+++ 20-Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
@@ -1,6 +1,10 @@
 package com.cybertek.entity;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
 
 @Entity
 @Table(name = "students")
@@ -14,5 +18,22 @@ public class Student {
     private String firstName;
     private String lastName;
     private String email;
+
+    @Transient  // used in order not to see the object on table
+    private String city;
+
+    @Temporal(TemporalType.DATE)
+    private Date birthDate;
+    @Temporal(TemporalType.TIME)
+    private Date birthTime;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date birthDateTime;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate localDate;
+    @Column(columnDefinition = "TIME")
+    private LocalTime localTime;
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime localDateTime;
 
 }
