64c68b5cb052e649002cc24482c426cf39d2787e
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-04-18 06:18:06 +0100

    ORM - Table Creation

--- 20-Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Car.java
+++ 20-Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Car.java
@@ -1,9 +1,18 @@
 package com.cybertek.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class Car {
 
     @Id
@@ -12,4 +21,8 @@ public class Car {
     private String make;
     private String model;
 
+    public Car(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
 }
