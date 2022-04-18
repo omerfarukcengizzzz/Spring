c420d8f4cd84a913b5357b413dcd3b4cdbae972f
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-04-18 06:23:12 +0100

    ORM - Table Creation

--- 20-Spring-09-ORM-TableCreation/src/main/java/com/cybertek/Application.java
+++ 20-Spring-09-ORM-TableCreation/src/main/java/com/cybertek/Application.java
@@ -1,13 +1,34 @@
 package com.cybertek;
 
+import com.cybertek.entity.Car;
+import com.cybertek.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class Application {
 
+	@Autowired
+	CarRepository carRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@PostConstruct	// it will run before the main method
+	public void dataInit() {
+
+		Car c1 = new Car("BMW", "M5");
+		Car c2 = new Car("MERCEDES", "GLA");
+		Car c3 = new Car("VW", "PASSAT");
+
+		carRepository.save(c1);
+		carRepository.save(c2);
+		carRepository.save(c3);
+
 	}
 
 }
