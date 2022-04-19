5c990d7d295f9268b362deb72cd4f6a7842fb474
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-04-19 02:44:10 +0100

    ORM - Mapping - ManyToOne

--- 22-Spring-09-ORM-Mapping-ManyToOne/src/main/java/com/cybertek/entity/Employee.java
+++ 22-Spring-09-ORM-Mapping-ManyToOne/src/main/java/com/cybertek/entity/Employee.java
@@ -27,9 +27,7 @@ public class Employee extends BaseEntity{
 
     private int salary;
 
-//    @OneToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST})
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JoinColumn(name = "dep_id")    // naming convention
     private Department department;
 
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
