95e2242e44143b7ec49fed733c89a846a6bc4273
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-04-19 01:05:02 +0100

    ORM - Mapping - OneToOne

--- 21-Spring-09-ORM-Mapping-OneToOne/src/main/java/com/cybertek/Application.java
+++ 21-Spring-09-ORM-Mapping-OneToOne/src/main/java/com/cybertek/Application.java
@@ -3,18 +3,11 @@ package com.cybertek;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.PostConstruct;
-
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@PostConstruct
-	public void dataInitializer() {
-
 	}
 
 }
---
+++ 21-Spring-09-ORM-Mapping-OneToOne/src/main/java/com/cybertek/bootstrap/DataGenerator.java
@@ -0,0 +1,14 @@
+package com.cybertek.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataGenerator implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+
+}
