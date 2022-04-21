11e2f1ac7cc6978334603e3d0564ee4f0178fd8b
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-04-21 12:35:24 +0100

    ORM - Mapping - OneToMany

--- 23-Spring-09-ORM-Mapping-OneToMany/src/main/java/com/cybertek/entity/Address.java
+++ 23-Spring-09-ORM-Mapping-OneToMany/src/main/java/com/cybertek/entity/Address.java
@@ -1,4 +1,15 @@
 package com.cybertek.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
