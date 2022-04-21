d1c7ec6f8f1b9e8c0047e26cf320dd5017d71685
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-04-21 16:31:03 +0100

    ORM - Mapping - ManyToMany

--- 24-Spring-09-ORM-Mapping-ManyToMany/src/main/java/com/cybertek/entity/Tag.java
+++ 24-Spring-09-ORM-Mapping-ManyToMany/src/main/java/com/cybertek/entity/Tag.java
@@ -5,6 +5,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @NoArgsConstructor
@@ -12,6 +15,13 @@ import javax.persistence.Entity;
 @Setter
 public class Tag {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String name;
 
+    public Tag(String name) {
+        this.name = name;
+    }
 }
