122c6913e26c023dadba3bba1735a9d1c3693eec
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-04-21 13:00:27 +0100

    ORM - Mapping - OneToMany

--- 23-Spring-09-ORM-Mapping-OneToMany/src/main/java/com/cybertek/entity/Person.java
+++ 23-Spring-09-ORM-Mapping-OneToMany/src/main/java/com/cybertek/entity/Person.java
@@ -14,7 +14,7 @@ public class Person {
     private String lastName;
 
 //    OPTION 1: Address is the super class
-    // if we map with @ManyToOne from Address class, we don't need to map it from here. (we don't need these lines)
+    // if we map with @ManyToOne from Address class, we don't need to map it from here. (we don't need these lines if we don't want to map as a bi-direction)
     @OneToMany(mappedBy = "person")
     private List<Address> addresses;
 
