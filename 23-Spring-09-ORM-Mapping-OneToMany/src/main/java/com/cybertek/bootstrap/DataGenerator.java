ce0b92a6bc43a7248ad1105b8a313fa180038428
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-04-21 16:20:00 +0100

    ORM - Mapping - OneToMany

--- 23-Spring-09-ORM-Mapping-OneToMany/src/main/java/com/cybertek/bootstrap/DataGenerator.java
+++ 23-Spring-09-ORM-Mapping-OneToMany/src/main/java/com/cybertek/bootstrap/DataGenerator.java
@@ -2,6 +2,7 @@ package com.cybertek.bootstrap;
 
 import com.cybertek.entity.Address;
 import com.cybertek.entity.Person;
+import com.cybertek.repository.AddressRepository;
 import com.cybertek.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -14,6 +15,8 @@ public class DataGenerator implements CommandLineRunner {
 
     @Autowired
     PersonRepository personRepository;
+    @Autowired
+    AddressRepository addressRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -26,9 +29,18 @@ public class DataGenerator implements CommandLineRunner {
         Address a2 = new Address("Elm St", "20222");
         Address a3 = new Address("Java St", "20223");
 
-        p1.setAddresses(Arrays.asList(a1, a2));
+//        OPTION 2:
+//        p1.setAddresses(Arrays.asList(a1, a2));
 
         personRepository.save(p1);
+
+        a1.setPerson(p1);
+        a2.setPerson(p1);
+        a3.setPerson(p1);
+
+        addressRepository.save(a1);
+        addressRepository.save(a2);
+        addressRepository.save(a3);
 
     }
 }
