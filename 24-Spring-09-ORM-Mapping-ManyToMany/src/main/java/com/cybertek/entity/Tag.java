f22e021cfa42da92ea256cb3edd4ce9414f82e6d
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-04-21 16:30:02 +0100

    ORM - Mapping - ManyToMany

---
+++ 24-Spring-09-ORM-Mapping-ManyToMany/src/main/java/com/cybertek/entity/Tag.java
@@ -0,0 +1,17 @@
+package com.cybertek.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Tag {
+
+
+
+}
