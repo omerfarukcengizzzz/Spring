2bb0621e46f8b80cb04c86d8e7cb4b3a7b93440f
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-04-26 13:53:25 +0100

    ORM - Lab - CinemaApp

--- 25-Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/MovieCinema.java
+++ 25-Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/MovieCinema.java
@@ -4,11 +4,25 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
 @Setter
 @Getter
 public class MovieCinema extends BaseEntity {
+
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime localDateTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+
+
+    public MovieCinema(LocalDateTime localDateTime) {
+        this.localDateTime = localDateTime;
+    }
 }
