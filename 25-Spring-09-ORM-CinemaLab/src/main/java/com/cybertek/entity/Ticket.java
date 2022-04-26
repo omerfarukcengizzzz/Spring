2c9a1c21127d61c0adf0e3420b079d5b12cfdbc5
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-04-26 14:54:46 +0100

    ORM - Lab - CinemaApp

--- 25-Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/Ticket.java
+++ 25-Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/Ticket.java
@@ -17,7 +17,7 @@ public class Ticket extends BaseEntity {
     private Integer rowNumber;
 
     @Column(columnDefinition = "TIMESTAMP")
-    private LocalDateTime localDateTime;
+    private LocalDateTime dateTime;
 
     @ManyToOne(fetch = FetchType.LAZY)
     private MovieCinema movieCinema;
