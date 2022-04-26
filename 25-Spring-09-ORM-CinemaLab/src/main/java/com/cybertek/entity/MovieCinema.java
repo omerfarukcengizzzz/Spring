9a28564979b7b9bc1328a689616db54b4253d28e
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-04-26 13:42:45 +0100

    ORM - Lab - CinemaApp

---
+++ 25-Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/MovieCinema.java
@@ -0,0 +1,14 @@
+package com.cybertek.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@NoArgsConstructor
+@Setter
+@Getter
+public class MovieCinema extends BaseEntity {
+}
