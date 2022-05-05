615cd9e5a13a03cc570aef1dfbef9ebefdb0fd14
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-05-05 16:31:06 +0100

    ORM - Derived Queries

--- 26-Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/Application.java
+++ 26-Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/Application.java
@@ -1,13 +1,31 @@
 package com.cybertek;
 
+import com.cybertek.repository.RegionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Autowired
+    RegionRepository regionRepository;
+
+    @PostConstruct
+    public void testRegions() {
+
+        System.out.println("----- REGIONS START -----");
+
+        System.out.println(regionRepository.findByCountry("Canada"));
+
+        System.out.println("----- REGIONS END -----");
+
     }
 
 }
