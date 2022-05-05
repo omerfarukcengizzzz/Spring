be9b7ab3bdad56dfa03b4b6760d637696d47c891
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-05-05 16:57:55 +0100

    ORM - Derived Queries

--- 26-Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/repository/DepartmentRepository.java
+++ 26-Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/repository/DepartmentRepository.java
@@ -4,6 +4,17 @@ import com.cybertek.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, String> {
+
+    //Display all the departments in "Furniture" department
+    List<Department> findByDepartment(String department);
+
+    // Dipslay all the departments in "Health" division
+    List<Department> findByDivision(String division);
+    List<Department> findByDivisionIs(String division);
+    List<Department> findByDivisionEquals(String division);
+
 }
