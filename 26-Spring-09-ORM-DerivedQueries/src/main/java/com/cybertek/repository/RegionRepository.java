f55757af675d8be36998b890c094c5f3efac9a7e
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-05-05 16:30:59 +0100

    ORM - Derived Queries

--- 26-Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/repository/RegionRepository.java
+++ 26-Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/repository/RegionRepository.java
@@ -4,6 +4,12 @@ import com.cybertek.entity.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RegionRepository extends JpaRepository<Region, Long> {
+
+    // Display all the regions in Canada
+    List<Region> findByCountry(String country);
+
 }
