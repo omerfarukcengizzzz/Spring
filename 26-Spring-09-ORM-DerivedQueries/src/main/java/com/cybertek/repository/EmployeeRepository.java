1c029f0cee27c39fc90e25a3c1da42cafbe2524c
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-05-05 18:44:37 +0100

    ORM - Derived Queries

--- 26-Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
+++ 26-Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
@@ -4,6 +4,7 @@ import com.cybertek.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -20,5 +21,23 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     // Display all the employees where lastname starts with ''
     List<Employee> findByLastNameStartingWith(String lastName);
+
+    // Display all the employees where the salary is higher than ''
+    List<Employee> findBySalaryGreaterThan(Integer salary);
+
+    // Display all the employees where the salary is less and equal than ''
+    List<Employee> findBySalaryLessThanEqual(Integer salary);
+
+    // Display all the employees that has been hired between "" and ""
+    List<Employee> findByHireDateBetween(LocalDate startDate, LocalDate endDate);
+
+    // Display all employees where salaries greater and equal to '' in order
+    List<Employee> findBySalaryGreaterThanEqualOrderBySalaryDesc(Integer salary);
+
+    // Display top unique 3 employees that is making less than ''
+    List<Employee> findDistinctTop3BySalaryLessThan(Integer salary);
+
+    // Display all the employees that do not have email address
+    List<Employee> findByEmailIsNull();
 
 }
