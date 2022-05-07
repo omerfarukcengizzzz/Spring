ec4f9a51b0139d5a90fd458d849cb013ba87c9ea
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-05-07 06:18:12 +0100

    ORM - JPQL - Named Queries

--- 27-Spring-09-ORM-JPQL-NamedQueries/src/main/java/com/cybertek/repository/RegionRepository.java
+++ 27-Spring-09-ORM-JPQL-NamedQueries/src/main/java/com/cybertek/repository/RegionRepository.java
@@ -2,11 +2,16 @@ package com.cybertek.repository;
 
 import com.cybertek.entity.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RegionRepository extends JpaRepository<Region, Long> {
 
-
+    // Native Query
+    @Query(value = "select * from regions where country = ?1", nativeQuery = true)
+    List<Region> getRegionByCountry(String country);
 
 }
