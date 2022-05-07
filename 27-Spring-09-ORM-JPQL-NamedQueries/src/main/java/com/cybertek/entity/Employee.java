e1c9870be4c2e3511767f7bbea0dd587aafbb87c
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-05-07 05:17:14 +0100

    ORM - JPQL - Named Queries

---
+++ 27-Spring-09-ORM-JPQL-NamedQueries/src/main/java/com/cybertek/entity/Employee.java
@@ -0,0 +1,49 @@
+package com.cybertek.entity;
+
+import com.cybertek.enums.Gender;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Employee extends BaseEntity {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate hireDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department")    // on the data.sql the name is defined as department, the default will be department_id
+    private Department department;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private Integer salary;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", hireDate=" + hireDate +
+                ", gender=" + gender +
+                ", salary=" + salary +
+                '}';
+    }
+}
---
+++ 27-Spring-09-ORM-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package com.cybertek.repository;
+
+import com.cybertek.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+
+
+}
