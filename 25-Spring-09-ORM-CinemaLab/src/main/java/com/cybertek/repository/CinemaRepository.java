184134e67059cd22e70f7ae1580a03ba95f4c533
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-05-09 14:53:30 +0100

    ORM - Lab - CinemaApp

--- 25-Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/CinemaRepository.java
+++ 25-Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/CinemaRepository.java
@@ -1,7 +1,6 @@
 package com.cybertek.repository;
 
 import com.cybertek.entity.Cinema;
-import com.cybertek.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
