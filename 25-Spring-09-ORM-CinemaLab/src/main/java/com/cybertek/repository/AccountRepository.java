d1e3fa5c1374c18e626d181965e7475ccdd3e800
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-05-09 12:15:44 +0100

    ORM - Lab - CinemaApp

--- 25-Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/AccountRepository.java
+++ 25-Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/AccountRepository.java
@@ -1,6 +1,7 @@
 package com.cybertek.repository;
 
 import com.cybertek.entity.Account;
+import com.cybertek.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,7 +13,21 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     // ------------------- DERIVED QUERIES ----------------------
 
     // Write a derived query to list all accounts with a specific country or state
+    List<Account> findByCountryOrState(String country, String state);
 
     // Write a derived query to list all accounts with age lower than or equal to a specific value
+    List<Account> findByAgeLessThanEqual(Integer age);
+
+    // Write a derived query to list all accounts with a specific role
+    List<Account> findByRole(UserRole role);
+
+    // Write a derived query to list all accounts between a range of ages
+    List<Account> findByAgeIsBetweenAndOrderByAgeDesc(Integer age1, Integer age2);
+
+    // Write a derived query to list all accounts where the beginning of the address contains the keyword
+    List<Account> findByAddressStartingWith(String prefix);
+
+    // Write a derived query to sort the list of accounts with age
+    List<Account> getAccountsByAgeOrderByAge();
 
 }
