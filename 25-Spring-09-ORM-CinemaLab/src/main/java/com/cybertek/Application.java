42cd0e5f3b9b6d5683a7a7024e3339c0b24b7d3c
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-05-10 10:00:06 +0100

    ORM - Lab - CinemaApp

--- 25-Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/Application.java
+++ 25-Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/Application.java
@@ -1,16 +1,18 @@
 package com.cybertek;
 
+import com.cybertek.enums.MovieState;
+import com.cybertek.enums.MovieType;
 import com.cybertek.enums.UserRole;
-import com.cybertek.repository.AccountRepository;
-import com.cybertek.repository.CinemaRepository;
-import com.cybertek.repository.GenreRepository;
-import com.cybertek.repository.MovieCinemaRepository;
+import com.cybertek.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
@@ -27,6 +29,8 @@ public class Application {
     GenreRepository genreRepository;
     @Autowired
     MovieCinemaRepository movieCinemaRepository;
+    @Autowired
+    MovieRepository movieRepository;
 
     @PostConstruct
     public void test() {
@@ -74,6 +78,20 @@ public class Application {
         System.out.println(movieCinemaRepository.countByCinemaId(2l));
         System.out.println(movieCinemaRepository.retrieveAllByLocationName("AMC Village 7"));
         System.out.println("-----------------------------------------------");
+
+        System.out.println("----------- MOVIE REPOSITORY -----------");
+        System.out.println(movieRepository.findByName("The Gentleman"));
+        System.out.println(movieRepository.findAllByPriceBetween(BigDecimal.valueOf(10), BigDecimal.valueOf(16)));
+        System.out.println(movieRepository.findByDurationIn(Arrays.asList(133, 135, 100, 129, 98)));
+        System.out.println(movieRepository.findByReleaseDateAfter(LocalDate.now().minusYears(2)));
+        System.out.println(movieRepository.findByStateAndType(MovieState.ACTIVE, MovieType.REGULAR));
+        System.out.println(movieRepository.retrieveAllByPricesBetween(BigDecimal.valueOf(10), BigDecimal.valueOf(16)));
+        System.out.println(movieRepository.retrieveAllMovieNames());
+        System.out.println(movieRepository.findByMovieName("Tenet"));
+        System.out.println(movieRepository.findByPriceBetween(BigDecimal.valueOf(10), BigDecimal.valueOf(20)));
+        System.out.println(movieRepository.findByDurationBetween(Arrays.asList(10, 40, 100, 150)));
+        System.out.println(movieRepository.findTop5ByPrice());
+        System.out.println("----------------------------------------");
 
     }
 
--- 25-Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/MovieRepository.java
+++ 25-Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/MovieRepository.java
@@ -1,8 +1,16 @@
 package com.cybertek.repository;
 
 import com.cybertek.entity.Movie;
+import com.cybertek.enums.MovieState;
+import com.cybertek.enums.MovieType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
@@ -10,29 +18,46 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
     // ------------------- DERIVED QUERIES ----------------------
 
     // Write a derived query to read a movie with a name
+    Optional<Movie> findByName(String name);
 
     // Write a derived query to list all movies between a range of prices
+    List<Movie> findAllByPriceBetween(BigDecimal price1, BigDecimal price2);
 
     // Write a derived query to list all movies where duration exists in the specific list of duration
+    List<Movie> findByDurationIn(List<Integer> duration);
 
     // Write a derived query to list all movies with higher than a specific release date
+    List<Movie> findByReleaseDateAfter(LocalDate date);
 
     // Write a derived query to list all movies with a specific state and type
+    List<Movie> findByStateAndType(MovieState state, MovieType type);
 
     // ------------------- JPQL QUERIES ----------------------
 
     // Write a JPQL query to list all movies between a range of prices
+    @Query("select m from Movie m where m.price between ?1 and ?2")
+    List<Movie> retrieveAllByPricesBetween(BigDecimal price1, BigDecimal price2);
 
     // Write a JPQL query that returns all movie names
+    @Query("select m.name from Movie m")
+    List<String> retrieveAllMovieNames();
 
     // ------------------- NATIVE QUERIES ----------------------
 
     // Write a native query that returns a movie by name
+    @Query(value = "select * from movie where name = ?1", nativeQuery = true)
+    Optional<Movie> findByMovieName(String name);
 
     // Write a native query that returns a list of movies in a specific range of prices
+    @Query(value = "select * from movie where price between ?1 and ?2", nativeQuery = true)
+    List<Movie> findByPriceBetween(BigDecimal price1, BigDecimal price2);
 
-    // Write a native query to returns all the movies where duration exists in the range of duration
+    // Write a native query to returns all the movies where duration between in the range list
+    @Query(value = "select * from movie where duration in ?1", nativeQuery = true)
+    List<Movie> findByDurationBetween(List<Integer> durations);
 
     // Write a native query to list the top 5 most expensive movies
+    @Query(value = "select * from movie order by price desc limit 5", nativeQuery = true)
+    List<Movie> findTop5ByPrice();
 
 }
