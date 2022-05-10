7c3ad1273d0b2c7f4ff620c513d6dc4c44199979
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-05-10 06:58:29 +0100

    ORM - Lab - CinemaApp

--- 25-Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/CinemaRepository.java
+++ 25-Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/CinemaRepository.java
@@ -12,42 +12,42 @@ import java.util.Optional;
 @Repository
 public interface CinemaRepository extends JpaRepository<Cinema, Long> {
 
-    // ------------------- DERIVED QUERIES ----------------------
-
-    // Write a derived query to get cinema with a specific name
-    List<Cinema> findByName(String name);
-
-    // Write a derived query to read sorted the top 3 cinemas that contains a specific sponsored name
-    List<Cinema> findTop3BySponsoredNameContaining(String infix);
-
-    // Write a derived query to list all cinemas in a specific country
-    List<Cinema> findAllByLocation_Country(String country);
-
-    // Write a derived query to list all cinemas with a specific name or sponsored name
-    List<Cinema> findByNameOrSponsoredName(String name, String sponsoredName);
-
-    // ------------------- JPQL QUERIES ----------------------
-
-    // Write a JPQL query to read the cinema name with a specific id
-    @Query("select c.name from Cinema c where c.id = :id")
-    Optional<Cinema> findCinemaById(@Param("id") Long id);
-
-    // ------------------- NATIVE QUERIES ----------------------
-
-    // Write a native query to read all cinemas by location country
-    @Query(value = "select * from cinema c join location l on l.location_id = c.location_id where l.country = ?1", nativeQuery = true)
-    List<Cinema> getAllByLocation(String country);
-
-    // Write a native query to read all cinemas by name or sponsored name containing a specific pattern
-    @Query(value = "select * from cinema where name ilike concat('%', '?1', '%') or sponsored_name ilike concat('%', '?1', '%')", nativeQuery = true)
-    List<Cinema> getCinemaByNameContainingOrSponsoredNameContaining(String infix);
-
-    // Write a native query to sort all cinemas by name
-    @Query(value = "select * from cinema order by name", nativeQuery = true)
-    List<Cinema> sortByName();
-
-    // Write a native query to distinct all cinemas by sponsored name
-    @Query(value = "select distinct sponsored_name from cinema", nativeQuery = true)
-    List<String> findDistinctBySponsoredName();
+//    // ------------------- DERIVED QUERIES ----------------------
+//
+//    // Write a derived query to get cinema with a specific name
+//    List<Cinema> findByName(String name);
+//
+//    // Write a derived query to read sorted the top 3 cinemas that contains a specific sponsored name
+//    List<Cinema> findTop3BySponsoredNameContaining(String infix);
+//
+//    // Write a derived query to list all cinemas in a specific country
+//    List<Cinema> findAllByLocation_Country(String country);
+//
+//    // Write a derived query to list all cinemas with a specific name or sponsored name
+//    List<Cinema> findByNameOrSponsoredName(String name, String sponsoredName);
+//
+//    // ------------------- JPQL QUERIES ----------------------
+//
+//    // Write a JPQL query to read the cinema name with a specific id
+//    @Query("select c.name from Cinema c where c.id = :id")
+//    Optional<Cinema> findCinemaById(@Param("id") Long id);
+//
+//    // ------------------- NATIVE QUERIES ----------------------
+//
+//    // Write a native query to read all cinemas by location country
+//    @Query(value = "select * from cinema c join location l on l.location_id = c.location_id where l.country = ?1", nativeQuery = true)
+//    List<Cinema> getAllByLocation(String country);
+//
+//    // Write a native query to read all cinemas by name or sponsored name containing a specific pattern
+//    @Query(value = "select * from cinema where name ilike concat('%', '?1', '%') or sponsored_name ilike concat('%', '?1', '%')", nativeQuery = true)
+//    List<Cinema> getCinemaByNameContainingOrSponsoredNameContaining(String infix);
+//
+//    // Write a native query to sort all cinemas by name
+//    @Query(value = "select * from cinema order by name", nativeQuery = true)
+//    List<Cinema> sortByName();
+//
+//    // Write a native query to distinct all cinemas by sponsored name
+//    @Query(value = "select distinct sponsored_name from cinema", nativeQuery = true)
+//    List<String> findDistinctBySponsoredName();
 
 }
--- 25-Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/GenreRepository.java
+++ 25-Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/GenreRepository.java
@@ -10,16 +10,16 @@ import java.util.List;
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
 
-    // ------------------- JPQL QUERIES ----------------------
-
-    // Write a JPQL query that returns all genres
-    @Query("select g from Genre g")
-    List<Genre> retrieveAllGenres();
-
-    // ------------------- NATIVE QUERIES ----------------------
-
-    // Write a native query that returns genres by containing name
-    @Query(value = "select * from genre where name ilike ('%', '?1', '%')", nativeQuery = true)
-    List<Genre> findByNameContaining(String infix);
+//    // ------------------- JPQL QUERIES ----------------------
+//
+//    // Write a JPQL query that returns all genres
+//    @Query("select g from Genre g")
+//    List<Genre> retrieveAllGenres();
+//
+//    // ------------------- NATIVE QUERIES ----------------------
+//
+//    // Write a native query that returns genres by containing name
+//    @Query(value = "select * from genre where name ilike ('%', '?1', '%')", nativeQuery = true)
+//    List<Genre> findByNameContaining(String infix);
 
 }
