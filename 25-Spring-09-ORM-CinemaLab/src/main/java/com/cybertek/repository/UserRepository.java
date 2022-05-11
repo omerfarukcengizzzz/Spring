9a093eb82a0e7709fc5601dd75f03f4649506fde
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-05-11 06:49:12 +0100

    ORM - Lab - CinemaApp

--- 25-Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/UserRepository.java
+++ 25-Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.cybertek.repository;
 
 import com.cybertek.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -29,19 +30,33 @@ public interface UserRepository extends JpaRepository<User, Long> {
     // ------------------- JPQL QUERIES ----------------------
 
     // Write a JPQL query that returns a user read by email?
+    @Query("select u from User u where u.email = ?1")
+    List<User> findAllByUserEmail(String email);
 
     // Write a JPQL query that returns a user read by username?
+    @Query("select u from User u where u.username = ?1")
+    List<User> findUserByUsername(String username);
 
     // Write a JPQL query that returns all users?
+    @Query("select u from User u")
+    List<User> retrieveAllUsers();
 
     // ------------------- NATIVE QUERIES ----------------------
 
     // Write a native query that returns all users that contain a specific name?
+    @Query(value = "select * from user_account ua join account_details ad on ua.account_details_id = ad.id where ad.name ilike concat ('%', ?1, '%')", nativeQuery = true)
+    List<User> retrieveAllByNameContaining(String name);
 
     // Write a native query that returns all users?
+    @Query(value = "select * from user_account", nativeQuery = true)
+    List<User> retrieveAll();
 
     // Write a native query that returns all users in the range of ages?
+    @Query(value = "select * from user_account ua join account_details ad on ua.account_details_id = ad.id where ad.age between ?1 and ?2",nativeQuery = true)
+    List<User> retrieveAllByAgeBetween(Integer age1, Integer age2);
 
     // Write a native query to read a user by email?
+    @Query(value = "select * from user_account where email = ?1", nativeQuery = true)
+    List<User> findByEmail(String email);
 
 }
