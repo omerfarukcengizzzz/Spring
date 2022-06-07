e6f45a5cbc6231f494a184b3c6a6eae786c81210
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-06-07 11:31:26 +0100

    Security - HttpBasic - Database

--- 31-Spring-10-Security-HttpBasic-Database/src/main/java/com/cybertek/bootstrap/DataGenerator.java
+++ 31-Spring-10-Security-HttpBasic-Database/src/main/java/com/cybertek/bootstrap/DataGenerator.java
@@ -4,6 +4,9 @@ import com.cybertek.entity.User;
 import com.cybertek.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -14,6 +17,13 @@ public class DataGenerator implements CommandLineRunner {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     public void run(String... args) throws Exception {
@@ -22,9 +32,9 @@ public class DataGenerator implements CommandLineRunner {
         userRepository.deleteAll();
 
         // Create users
-        User employee = new User("omer", "omer123", "USER", "");
-        User admin = new User("admin", "admin123", "ADMIN", "ACCESS_TEST1, ACCESS_TEST2");
-        User manager = new User("manager", "manager123", "MANAGER", "ACCESS_TEST1");
+        User employee = new User("omer", passwordEncoder.encode("omer123"), "USER", "");
+        User admin = new User("admin", passwordEncoder.encode("admin123"), "ADMIN", "ACCESS_TEST1, ACCESS_TEST2");
+        User manager = new User("manager", passwordEncoder.encode("manager123"), "MANAGER", "ACCESS_TEST1");
 
         List<User> userList = Arrays.asList(employee, admin, manager);
 
