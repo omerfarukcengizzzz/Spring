a56e6dff19f6215cb23eb6bee79350568f192224
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-06-07 10:14:32 +0100

    Security - HttpBasic - Authentication Role Base

--- 30-Spring-10-Security-HttpBasic-AuthRoleBase/src/main/java/com/cybertek/security/SecurityConfiguration.java
+++ 30-Spring-10-Security-HttpBasic-AuthRoleBase/src/main/java/com/cybertek/security/SecurityConfiguration.java
@@ -17,6 +17,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()    // request should be authorized
+                .antMatchers("index.html").permitAll()
+                .antMatchers("/profile/**").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/manager/**").hasRole("MANAGER")
                 .anyRequest().authenticated()   // incoming request should be authenticated
                 .and()
                 .httpBasic();   // perform basic http authentication
@@ -26,13 +30,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("admin")
-                .password(passwordEncoder().encode("admin123"))
-                .roles("ADMIN")
+                .withUser("admin").password(passwordEncoder().encode("admin123")).roles("ADMIN")
                 .and()
-                .withUser("omer")
-                .password(passwordEncoder().encode("omer123"))
-                .roles("USER");
+                .withUser("omer").password(passwordEncoder().encode("omer123")).roles("USER")
+                .and()
+                .withUser("manager").password(passwordEncoder().encode("manager123")).roles("MANAGER");
     }
 
     @Bean
