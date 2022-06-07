4fc98933ff0a9969770d06a2b0711ddfaf471598
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-06-07 14:13:53 +0100

    Security - HttpBasic - Database

--- 31-Spring-10-Security-HttpBasic-Database/src/main/java/com/cybertek/bootstrap/DataGenerator.java
+++ 31-Spring-10-Security-HttpBasic-Database/src/main/java/com/cybertek/bootstrap/DataGenerator.java
@@ -4,8 +4,6 @@ import com.cybertek.entity.User;
 import com.cybertek.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
