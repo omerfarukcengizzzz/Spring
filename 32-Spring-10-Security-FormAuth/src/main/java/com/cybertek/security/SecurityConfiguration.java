90891bb4c2b9c84cf9a08225940ffc0fd1ae898e
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-06-08 17:21:01 +0100

    Security - Form Authorization

--- 32-Spring-10-Security-FormAuth/src/main/java/com/cybertek/security/SecurityConfiguration.java
+++ 32-Spring-10-Security-FormAuth/src/main/java/com/cybertek/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.cybertek.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +13,9 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserPrincipalDetailsService userPrincipalDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -30,8 +34,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/login?logout=true")
-                .permitAll();
-
+                .permitAll()
+                .and()
+                .rememberMe()
+                .tokenValiditySeconds(120)  // this feature helps the user whether he lost the JSESSION(java session id) from cookies or not,  he will have a unique session id for () seconds.
+                .key("cybertekSecret")      // any key
+                .userDetailsService(userPrincipalDetailsService);   // we have to define which user are we working with in order to remember
     }
 
     @Bean
