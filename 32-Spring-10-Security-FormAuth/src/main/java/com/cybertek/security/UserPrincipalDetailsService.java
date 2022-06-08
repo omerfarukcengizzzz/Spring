a14787915a7af9c51f92553f6eeb156f9d50bb00
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-06-08 15:59:02 +0100

    Security - Form Authorization

---
+++ 32-Spring-10-Security-FormAuth/src/main/java/com/cybertek/security/UserPrincipalDetailsService.java
@@ -0,0 +1,25 @@
+package com.cybertek.security;
+
+import com.cybertek.entity.User;
+import com.cybertek.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserPrincipalDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        UserPrincipal userPrincipal = new UserPrincipal(user);
+
+        return userPrincipal;
+    }
+
+}
