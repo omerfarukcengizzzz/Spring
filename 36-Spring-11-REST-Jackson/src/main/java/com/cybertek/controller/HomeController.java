be46c8802b81de9f01bd3fc5923104b18f404f2a
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-07-11 00:52:13 +0100

    REST - Jackson

--- 36-Spring-11-REST-Jackson/src/main/java/com/cybertek/controller/HomeController.java
+++ 36-Spring-11-REST-Jackson/src/main/java/com/cybertek/controller/HomeController.java
@@ -1,4 +1,26 @@
 package com.cybertek.controller;
 
+import com.cybertek.entity.Account;
+import com.cybertek.repository.AccountRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
 public class HomeController {
+
+    // Dependencies
+    private AccountRepository accountRepository;
+
+    // Account Controller
+    @GetMapping("/getAccounts")
+    public ResponseEntity<List<Account>> getAllAccount() {
+        return ResponseEntity
+                .ok(accountRepository.getAllAccounts());
+    }
+
 }
