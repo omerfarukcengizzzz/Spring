6966ff1fac768769e82d1bb2468a9b9708d4d0fb
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-07-11 17:38:05 +0100

    REST - Jackson

--- 36-Spring-11-REST-Jackson/src/main/java/com/cybertek/implementation/UserServiceImpl.java
+++ 36-Spring-11-REST-Jackson/src/main/java/com/cybertek/implementation/UserServiceImpl.java
@@ -18,7 +18,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User findByEmail(String email) {
-        return null;
+        return userRepository.findUserByEmail(email);
     }
 
     @Override
