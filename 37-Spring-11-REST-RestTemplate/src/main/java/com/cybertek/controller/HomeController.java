a6310f0f75e2362cf6160ed173c1c97b7e26075d
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-09-18 14:23:29 -0700

    REST - Rest Template

--- 37-Spring-11-REST-RestTemplate/src/main/java/com/cybertek/controller/HomeController.java
+++ 37-Spring-11-REST-RestTemplate/src/main/java/com/cybertek/controller/HomeController.java
@@ -1,4 +1,25 @@
 package com.cybertek.controller;
 
+import com.cybertek.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
 public class HomeController {
+
+    final String URI = "https://jsonplaceholder.typicode.com/users";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping
+    public User[] readAllUsers() {
+        ResponseEntity<User[]> responseEntity = restTemplate.getForEntity(URI, User[].class);
+
+        return responseEntity.getBody();
+    }
+
 }
