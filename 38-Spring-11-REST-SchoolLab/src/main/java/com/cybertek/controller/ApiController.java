65247d850fdeef76474b140a438f6eac58b74a3d
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-09-21 11:49:31 -0700

    REST - School Lab

--- 38-Spring-11-REST-SchoolLab/src/main/java/com/cybertek/controller/ApiController.java
+++ 38-Spring-11-REST-SchoolLab/src/main/java/com/cybertek/controller/ApiController.java
@@ -1,12 +1,10 @@
 package com.cybertek.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Arrays;
 
 @RestController
 public class ApiController {
@@ -15,20 +13,11 @@ public class ApiController {
     private RestTemplate restTemplate;
 
     @GetMapping("/test")
-    public ResponseEntity<Object> consumePostsFromDummyApi() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        headers.set("access_key", "ed60609d6e1582bafaa71464a763eeb1");
-
+    public Object readUserById(@PathVariable("id") Integer id) {
         String city = "Henderson";
+        String URL = "http://api.weatherstack.com/current?access_key=ed60609d6e1582bafaa71464a763eeb1&query=" + city;
 
-        headers.set("query", city);
-
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-
-        ResponseEntity<Object> response = restTemplate.exchange("http://api.weatherstack.com/", HttpMethod.GET, entity, Object.class);
-
-        return response;
+        return restTemplate.getForObject(URL, Object.class, id);
     }
 
 }
