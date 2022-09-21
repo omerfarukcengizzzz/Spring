43d141a1c04882d6b27ab37df8826e567e7297b1
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-09-21 12:00:43 -0700

    REST - School Lab

--- 38-Spring-11-REST-SchoolLab/src/main/java/com/cybertek/controller/ApiController.java
+++ 38-Spring-11-REST-SchoolLab/src/main/java/com/cybertek/controller/ApiController.java
@@ -1,22 +1,35 @@
 package com.cybertek.controller;
 
+import com.cybertek.model.Teacher;
+import com.cybertek.repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 public class ApiController {
 
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private TeacherRepository teacherRepository;
 
-    @GetMapping("/test")
-    public Object readUserById() {
-        String city = "Henderson";
+    @GetMapping("/{city}")
+    public Object readUserById(@PathVariable("city") String city) {
         String URL = "http://api.weatherstack.com/current?access_key=ed60609d6e1582bafaa71464a763eeb1&query=" + city;
 
         return restTemplate.getForObject(URL, Object.class);
     }
+
+    @GetMapping("/teachers")
+    public List<Teacher> readAllTeachers() {
+        return teacherRepository.findAll();
+    }
+
+
 
 }
