fd81745c7dc672a24442a57400d53ed919d3bd16
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-09-20 22:04:03 -0700

    REST - School Lab

--- 38-Spring-11-REST-SchoolLab/src/main/java/com/cybertek/controller/ApiController.java
+++ 38-Spring-11-REST-SchoolLab/src/main/java/com/cybertek/controller/ApiController.java
@@ -3,7 +3,6 @@ package com.cybertek.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
