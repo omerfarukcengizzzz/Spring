5f85ffdc8544369d986f080cc5fd98e4076c2dbb
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-09-21 12:10:11 -0700

    REST - School Lab

--- 38-Spring-11-REST-SchoolLab/src/main/java/com/cybertek/controller/ApiController.java
+++ 38-Spring-11-REST-SchoolLab/src/main/java/com/cybertek/controller/ApiController.java
@@ -1,8 +1,10 @@
 package com.cybertek.controller;
 
+import com.cybertek.enums.ResponseWrapper;
 import com.cybertek.model.Teacher;
 import com.cybertek.repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +30,12 @@ public class ApiController {
     @GetMapping("/teachers")
     public List<Teacher> readAllTeachers() {
         return teacherRepository.findAll();
+    }
+
+    @GetMapping("/students")
+    public ResponseEntity<ResponseWrapper> readAllStudents() {
+        return ResponseEntity
+                .ok(new ResponseWrapper("Students are successfully retrieved"));
     }
 
 
