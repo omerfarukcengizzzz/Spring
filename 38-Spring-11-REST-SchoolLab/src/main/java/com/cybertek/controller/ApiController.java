4b43556b525338679364a95da9712791e6f3db57
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-09-22 13:54:47 -0700

    REST - School Lab

--- 38-Spring-11-REST-SchoolLab/src/main/java/com/cybertek/controller/ApiController.java
+++ 38-Spring-11-REST-SchoolLab/src/main/java/com/cybertek/controller/ApiController.java
@@ -1,7 +1,9 @@
 package com.cybertek.controller;
 
 import com.cybertek.enums.ResponseWrapper;
+import com.cybertek.model.Address;
 import com.cybertek.model.Teacher;
+import com.cybertek.repository.AddressRepository;
 import com.cybertek.repository.ParentRepository;
 import com.cybertek.repository.StudentRepository;
 import com.cybertek.repository.TeacherRepository;
@@ -9,12 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ApiController {
@@ -28,6 +29,9 @@ public class ApiController {
     private StudentRepository studentRepository;
     @Autowired
     private ParentRepository parentRepository;
+    @Autowired
+    private AddressRepository addressRepository;
+
 
     @GetMapping("/{city}")
     public Object readUserById(@PathVariable("city") String city) {
@@ -55,6 +59,20 @@ public class ApiController {
 
         return ResponseEntity.status(HttpStatus.ACCEPTED)
                 .body(responseWrapper);
+    }
+
+    @PutMapping("/address/{id}")
+    public Address updateAddress(@PathVariable long id, @RequestBody Address address) throws Exception {
+        Optional<Address> foundAddress = addressRepository.findById(id);
+
+        if (!foundAddress.isPresent()) {
+            throw new Exception("Address does not exist!");
+        }
+
+        address.setCurrentTemperature(new Address().consumeTemp(address.getCity()));
+        address.setId(foundAddress.get().getId());
+
+        return addressRepository.save(address);
     }
 
 
