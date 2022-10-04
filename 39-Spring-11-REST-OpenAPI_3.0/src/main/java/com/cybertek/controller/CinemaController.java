a5f2a72dcd092e63e87a62da70ad54ceaf67f947
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-10-04 16:44:48 -0700

    REST - OpenAPI 3.0

---
+++ 39-Spring-11-REST-OpenAPI_3.0/src/main/java/com/cybertek/controller/CinemaController.java
@@ -0,0 +1,24 @@
+package com.cybertek.controller;
+
+import com.cybertek.entity.Cinema;
+import com.cybertek.repository.CinemaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cinema")
+public class CinemaController {
+
+    @Autowired
+    private CinemaRepository cinemaRepository;
+
+    @GetMapping("/read-all")
+    public List<Cinema> readAllCinemas() {
+        return cinemaRepository.findAll();
+    }
+
+}
