84c620a0ce2a22be478381d2f0e5c8295e64e58a
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-10-05 15:31:16 -0700

    REST - OpenAPI 3.0

--- 39-Spring-11-REST-OpenAPI_3.0/src/main/java/com/cybertek/controller/CinemaController.java
+++ 39-Spring-11-REST-OpenAPI_3.0/src/main/java/com/cybertek/controller/CinemaController.java
@@ -2,6 +2,7 @@ package com.cybertek.controller;
 
 import com.cybertek.entity.Cinema;
 import com.cybertek.repository.CinemaRepository;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/cinema")
+@Tag(name = "Cinema", description = "Cinema API")
 public class CinemaController {
 
     @Autowired
--- 39-Spring-11-REST-OpenAPI_3.0/src/main/java/com/cybertek/controller/UserController.java
+++ 39-Spring-11-REST-OpenAPI_3.0/src/main/java/com/cybertek/controller/UserController.java
@@ -2,6 +2,8 @@ package com.cybertek.controller;
 
 import com.cybertek.entity.User;
 import com.cybertek.repository.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +13,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@Tag(name = "User", description = "User API")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
     @GetMapping
+    @Operation(summary = "Get all users from the database")
     public List<User> readAllUsers() {
         return userRepository.findAll();
     }
