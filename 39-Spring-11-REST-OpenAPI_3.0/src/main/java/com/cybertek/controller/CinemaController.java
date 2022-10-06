82f45870d543258d3545fb5d1e2ce40cc4eab885
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-10-06 12:35:48 -0700

    REST - OpenAPI 3.0

--- 39-Spring-11-REST-OpenAPI_3.0/src/main/java/com/cybertek/controller/CinemaController.java
+++ 39-Spring-11-REST-OpenAPI_3.0/src/main/java/com/cybertek/controller/CinemaController.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +33,16 @@ public class CinemaController {
     })
     public List<Cinema> readAllCinemas() {
         return cinemaRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Get cinema by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cinema is successfully retrieved", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Cinema cannot be retrieved", content = @Content)
+    })
+    public Cinema getCinemaById(@PathVariable Long id) {
+        return cinemaRepository.findCinemaById(id).get();
     }
 
 }
