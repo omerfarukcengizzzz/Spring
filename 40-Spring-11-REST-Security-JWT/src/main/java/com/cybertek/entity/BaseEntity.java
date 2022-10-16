83fb2e4020047f6dc0139af2ef4670c89143dd23
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-10-16 13:06:05 -0700

    Security - JWT

--- 40-Spring-11-REST-Security-JWT/src/main/java/com/cybertek/entity/BaseEntity.java
+++ 40-Spring-11-REST-Security-JWT/src/main/java/com/cybertek/entity/BaseEntity.java
@@ -1,11 +1,15 @@
 package com.cybertek.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Date;
 
 @MappedSuperclass
+@Getter
+@Setter
 public class BaseEntity {
 
     @Id
