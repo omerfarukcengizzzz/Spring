12c8865d186a15bd742fa6fea9412ddd489c5d71
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-10-17 13:33:33 -0700

    Security - JWT

---
+++ 40-Spring-11-REST-Security-JWT/src/main/java/com/cybertek/config/WebSecurityConfig.java
@@ -0,0 +1,15 @@
+package com.cybertek.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
