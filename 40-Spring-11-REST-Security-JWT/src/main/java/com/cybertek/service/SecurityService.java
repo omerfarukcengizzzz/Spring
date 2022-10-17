3fb14d4d0bf00192b5c857cfc4036add6a439079
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-10-17 14:23:07 -0700

    Security - JWT

---
+++ 40-Spring-11-REST-Security-JWT/src/main/java/com/cybertek/service/SecurityService.java
@@ -0,0 +1,23 @@
+package com.cybertek.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityService implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+
+
+        return null;
+    }
+
+}
