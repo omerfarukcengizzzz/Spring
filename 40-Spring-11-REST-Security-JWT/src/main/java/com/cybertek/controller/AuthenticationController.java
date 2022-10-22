31dcef7b52b6ab3dd99cc7c39abfc850e8fa6b2c
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-10-22 16:11:53 -0700

    Security - JWT

--- 40-Spring-11-REST-Security-JWT/src/main/java/com/cybertek/controller/AuthenticationController.java
+++ 40-Spring-11-REST-Security-JWT/src/main/java/com/cybertek/controller/AuthenticationController.java
@@ -2,7 +2,13 @@ package com.cybertek.controller;
 
 import com.cybertek.entity.AuthenticationRequest;
 import com.cybertek.entity.ResponseWrapper;
+import com.cybertek.entity.User;
+import com.cybertek.service.UserService;
+import com.cybertek.util.JWTUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +16,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthenticationController {
 
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JWTUtil jwtUtil;
+
     @PostMapping("/authenticate")
     public ResponseEntity<ResponseWrapper> login(@RequestBody AuthenticationRequest request) {
 
         String username = request.getUsername();
         String password = request.getPassword();
 
+        User foundUser = userService.readByUsername(username);
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+
+        authenticationManager.authenticate(authenticationToken);
+
+        String jwt = jwtUtil.generateToken(foundUser);
+
+        return ResponseEntity
+                .ok(new ResponseWrapper("Login successful!", jwt));
     }
 
 }
