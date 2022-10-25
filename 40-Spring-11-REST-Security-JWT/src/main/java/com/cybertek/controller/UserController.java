07d522a3759ea542c11cf78082afa032b0ac7968
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-10-24 20:27:15 -0700

    Security - JWT

--- 40-Spring-11-REST-Security-JWT/src/main/java/com/cybertek/controller/UserController.java
+++ 40-Spring-11-REST-Security-JWT/src/main/java/com/cybertek/controller/UserController.java
@@ -5,6 +5,7 @@ import com.cybertek.entity.User;
 import com.cybertek.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@EnableGlobalMethodSecurity(prePostEnabled = true)  // this annotation is activating authorization in this class
 public class UserController {
 
     @Autowired
