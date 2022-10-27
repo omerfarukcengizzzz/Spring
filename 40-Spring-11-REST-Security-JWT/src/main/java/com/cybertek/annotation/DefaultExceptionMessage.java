955d103873d38fad59a3808f537d7d4d024fc4ad
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-10-27 12:23:48 -0700

    Security - JWT

--- 40-Spring-11-REST-Security-JWT/src/main/java/com/cybertek/annotation/DefaultExceptionMessage.java
+++ 40-Spring-11-REST-Security-JWT/src/main/java/com/cybertek/annotation/DefaultExceptionMessage.java
@@ -1,4 +1,14 @@
 package com.cybertek.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD) // where do you want it to be applicable
+@Retention(RetentionPolicy.RUNTIME) // when will it be active
 public @interface DefaultExceptionMessage {
+
+    String defaultMessage() default "";
+
 }
