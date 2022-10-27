4f284d5f78884608fb7b6bf7c492327d00667e12
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-10-27 12:49:07 -0700

    Security - JWT

--- 40-Spring-11-REST-Security-JWT/src/main/java/com/cybertek/exception/ExceptionMessageHandler.java
+++ 40-Spring-11-REST-Security-JWT/src/main/java/com/cybertek/exception/ExceptionMessageHandler.java
@@ -1,4 +1,23 @@
 package com.cybertek.exception;
 
+import com.cybertek.entity.ResponseWrapper;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice   // this annotation means that, if you want to use this class you don't need to create an object for it
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class ExceptionMessageHandler {
+
+    @ExceptionHandler(ServiceException.class)
+    public ResponseEntity<ResponseWrapper> serviceException(ServiceException se) {
+
+        String message = se.getMessage();
+
+        return new ResponseEntity<>(ResponseWrapper.builder().success(false).code(HttpStatus.CONFLICT.value()).message(message).build(), HttpStatus.CONFLICT);
+    }
+
 }
