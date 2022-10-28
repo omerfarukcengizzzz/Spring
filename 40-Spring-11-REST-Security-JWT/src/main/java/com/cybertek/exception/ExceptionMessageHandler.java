8e09a49b2b2353e895cb6f6231807b06c7d751e3
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-10-27 17:29:52 -0700

    Security - JWT

--- 40-Spring-11-REST-Security-JWT/src/main/java/com/cybertek/exception/ExceptionMessageHandler.java
+++ 40-Spring-11-REST-Security-JWT/src/main/java/com/cybertek/exception/ExceptionMessageHandler.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.nio.file.AccessDeniedException;
+
 @RestControllerAdvice   // this annotation means that, if you want to use this class you don't need to create an object for it
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class ExceptionMessageHandler {
@@ -18,6 +20,14 @@ public class ExceptionMessageHandler {
         String message = se.getMessage();
 
         return new ResponseEntity<>(ResponseWrapper.builder().success(false).code(HttpStatus.CONFLICT.value()).message(message).build(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ResponseWrapper> accessDeniedException(ServiceException se) {
+        String message = se.getMessage();
+
+        return new ResponseEntity<>(ResponseWrapper.builder().success(false).code(HttpStatus.FORBIDDEN.value()).message(message).build(), HttpStatus.FORBIDDEN);
+
     }
 
 }
