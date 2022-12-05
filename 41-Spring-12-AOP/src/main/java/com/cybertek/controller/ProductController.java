f7d77bce43cecff914ad8060c63447749de0278f
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-12-04 17:46:43 -0800

    Spring - AOP

---
+++ 41-Spring-12-AOP/src/main/java/com/cybertek/controller/ProductController.java
@@ -0,0 +1,89 @@
+package com.cybertek.controller;
+
+import com.cybertek.entity.Product;
+import com.cybertek.entity.ResponseWrapper;
+import com.cybertek.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable("id") Long id) {
+        return ResponseEntity
+                .ok(productService.getProduct(id));
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Product>> getProducts() {
+        HttpHeaders responseHttpHeaders = new HttpHeaders();
+
+        responseHttpHeaders.set("Version", "Cybertek.v1");
+        responseHttpHeaders.set("Operation", "Get List");
+
+        return ResponseEntity
+                .ok()
+                .headers(responseHttpHeaders)
+                .body(productService.getProducts());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<List<Product>> deleteProduct(@PathVariable("id") Long id) {
+        return ResponseEntity
+                .ok(productService.delete(id));
+    }
+
+    @PostMapping()
+    public ResponseEntity<List<Product>> createProduct(@RequestBody Product product) {
+        List<Product> set = productService.createProduct(product);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("Version", "Cybertek.v1")
+                .header("Operation", "Create Product")
+                .body(set);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<List<Product>> updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        List<Product> list = productService.updateProduct(id, product);
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+
+        map.add("Version", "Cybertek.v1");
+        map.add("Operation", "Update Product");
+
+        return new ResponseEntity<>(list, map, HttpStatus.OK);
+    }
+
+    @GetMapping("/read")
+    public ResponseEntity<ResponseWrapper> readAllProducts() {
+        return ResponseEntity
+                .ok(new ResponseWrapper("Products successfully retrieved", productService.getProducts()));
+    }
+
+    @DeleteMapping("/delete1/{id}")
+    public ResponseEntity<ResponseWrapper> delete1(@PathVariable Long id) {
+        return ResponseEntity
+                .ok(new ResponseWrapper("Product successfully deleted"));
+    }
+
+    @DeleteMapping("/delete2/{id}")
+    public ResponseEntity<ResponseWrapper> delete2(@PathVariable Long id) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ResponseWrapper("Product successfully deleted"));
+    }
+
+}
