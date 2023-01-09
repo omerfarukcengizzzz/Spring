937bfda65dbaadf7b10b59d51b92dde827ec4d05
Omer Faruk <omerfarukcengizzzz@gmail.com>
2023-01-09 10:42:15 -0800

    Unit Testing - JUnit5

--- 42-Spring-13-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
+++ 42-Spring-13-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -5,7 +5,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
 
     @Test
+    void testCase1() {
+        assertEquals(20, 20);
+    }
+
+    @Test
     void add() {
+        int actual = Calculator.add(2, 3);
+        assertEquals(5, actual);
     }
 
 }
